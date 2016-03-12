1072b8fe5e1b17686093f17e0300571757e25ef4
Vergil <vergil@vip.163.com>
2016-03-13 07:24:05 +0800

    update

---
+++ src/ClientProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace VergilLai\UcClient;
+
+use Validator;
+use Illuminate\Support\ServiceProvider;
+
+
+class ClientProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/config/ucenter.php' => config_path('ucenter.php')
+        ]);
+
+        Validator::extend('uc_username', '\\VergilLai\\UcClient\\Validator@usernameValidate');
+        Validator::extend('uc_email', '\\VergilLai\\UcClient\\Validator@emailValidate');
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('uc-client', function($app) {
+            return new Client();
+        });
+    }
+}
