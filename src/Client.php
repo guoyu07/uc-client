b76237ed6f50b92bfc15406fad5cbf3db12881e9
Vergil <vergil@vip.163.com>
2016-03-13 07:31:11 +0800

    user login

--- src/Client.php
+++ src/Client.php
@@ -3,7 +3,6 @@
 namespace VergilLai\UcClient;
 
 use Config;
-<<<<<<< HEAD
 use Request;
 use Validator;
 use VergilLai\UcClient\Exceptions\UcException;
@@ -26,12 +25,6 @@ class Client
      */
     protected $config;
 
-=======
-
-class Client
-{
->>>>>>> 87ebeaa0a56d3b7371e43504e044a354567ef888
-
     public function __construct()
     {
         if (!defined('UC_API')) {
@@ -50,8 +43,6 @@ class Client
             define('UC_IP', $config['ip']);
             define('UC_APPID', $config['appid']);
             define('UC_PPP', $config['ppp']);
-<<<<<<< HEAD
-
             require __DIR__ . '/../uc_client/client.php';
         }
     }
@@ -196,23 +187,6 @@ class Client
         $return = $this->apiPost('user', 'login', $params);
         var_dump(xml_unserialize($return));
 
-=======
-        }
-    }
-
-    public function __call($function, $arguments)
-    {
-        if (function_exists($function)) {
-            return call_user_func_array($function, $arguments);
-        } else {
-            throw new \RuntimeException("UcClient Method Not Found.");
-        }
-    }
-
-    public function test()
-    {
-        echo __METHOD__;
->>>>>>> 87ebeaa0a56d3b7371e43504e044a354567ef888
     }
 
 
--- src/ClientProvider.php
+++ src/ClientProvider.php
@@ -2,15 +2,9 @@
 
 namespace VergilLai\UcClient;
 
-<<<<<<< HEAD
 use Validator;
 use Illuminate\Support\ServiceProvider;
 
-
-=======
-use Illuminate\Support\ServiceProvider;
-
->>>>>>> 87ebeaa0a56d3b7371e43504e044a354567ef888
 class ClientProvider extends ServiceProvider
 {
     /**
@@ -23,12 +17,9 @@ class ClientProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/config/ucenter.php' => config_path('ucenter.php')
         ]);
-<<<<<<< HEAD
 
         Validator::extend('uc_username', '\\VergilLai\\UcClient\\Validator@usernameValidate');
         Validator::extend('uc_email', '\\VergilLai\\UcClient\\Validator@emailValidate');
-=======
->>>>>>> 87ebeaa0a56d3b7371e43504e044a354567ef888
     }
 
     /**
