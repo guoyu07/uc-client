fa8a1742a120fdb25490150d8e529a308a7a4c07
Vergil <vergil@vip.163.com>
2016-03-13 18:05:51 +0800

    user api complete

--- src/Client.php
+++ src/Client.php
@@ -124,6 +124,10 @@ class Client
 
     /**
      * 用户注册
+     * 本接口函数用于新用户的注册。用户名、密码、Email 为一个用户在 UCenter 的基本数据，
+     * 提交后 UCenter 会按照注册设置和词语过滤的规则检测用户名和 Email 的格式是否正确合法，
+     * 如果正确则返回注册后的用户 ID，否则返回相应的错误信息。
+     *
      * @param string $username      用户名
      * @param string $password      密码
      * @param string $email         电子邮件
@@ -181,6 +185,8 @@ class Client
 
     /**
      * 用户登录
+     * 本接口函数用于用户的登录验证，用户名及密码正确无误则返回用户在 UCenter 的基本数据，
+     * 否则返回相应的错误信息。如果应用程序是升级过来的，并且当前登录用户和已有用户重名，那么返回的数组中 [4] 的值将返回 1。
      *
      * @param string $username      用户名 / 用户 ID / 用户 E-mail
      * @param string $password      密码
@@ -223,6 +229,8 @@ class Client
 
     /**
      * 获取用户数据
+     * 本接口函数用于获取用户在 UCenter 的基本数据，如用户不存在，返回值为 integer 的数值 0。
+     *
      * @param string $username  用户名
      * @param int $isuid        是否使用用户 ID获取
      *                          1:使用用户 ID获取
@@ -243,6 +251,9 @@ class Client
 
     /**
      * 更新用户资料
+     * 本接口函数用于更新用户资料。更新资料需验证用户的原密码是否正确，除非指定 ignoreoldpw 为 1。
+     * 如果只修改 Email 不修改密码，可让 newpw 为空；同理如果只修改密码不修改 Email，可让 email 为空。
+     *
      * @param string $username      用户名
      * @param string $oldpw         旧密码
      * @param string $newpw         新密码，如不修改为空
@@ -314,6 +325,11 @@ class Client
 
     /**
      * 同步登录
+     * 如果当前应用程序在 UCenter 中设置允许同步登录，那么本接口函数会通知其他设置了同步登录的应用程序登录，
+     * 把返回的 HTML 输出在页面中即可完成对其它应用程序的通知。输出的 HTML 中包含执行远程的 javascript 脚本，
+     * 请让页面在此脚本运行完毕后再进行跳转操作，否则可能会导致无法同步登录成功。同时要保证同步登录的正确有效，
+     * 请保证其他应用程序的 Cookie 域和 Cookie 路径设置正确。
+     *
      * @param int $uid
      * @return string 同步登录的 HTML 代码
      */
@@ -324,6 +340,11 @@ class Client
 
     /**
      * 同步退出
+     * 如果当前应用程序在 UCenter 中设置允许同步登录，那么本接口函数会通知其他设置了同步登录的应用程序退出登录，
+     * 把返回的 HTML 输出在页面中即可完成其它应用程序的通知。输出的 HTML 中包含执行远程的 javascript 脚本，
+     * 请让页面在此脚本运行完毕后再进行跳转操作，否则可能会导致无法同步退出登录。同时要保证同步退出登录的正确有效，
+     * 请保证其他应用程序的 Cookie 域和 Cookie 路径设置正确。
+     *
      * @return string 同步退出的 HTML 代码
      */
     public function userSyncLogout()
@@ -333,6 +354,8 @@ class Client
 
     /**
      * 检查 Email 地址
+     * 本接口函数用于检查用户输入的 Email 的合法性。
+     *
      * @param string $email Email
      * @return boolean
      */
@@ -368,6 +391,8 @@ class Client
 
     /**
      * 检查用户名
+     * 本接口函数用于检查用户输入的用户名的合法性。
+     *
      * @param string $username
      * @return boolean
      */
@@ -403,6 +428,8 @@ class Client
 
     /**
      * 添加保护用户
+     * 本接口函数用于添加被保护的用户。
+     *
      * @param string|array $username 保护用户名
      * @param string $admin 操作的管理员
      * @return boolean
@@ -415,6 +442,8 @@ class Client
 
     /**
      * 删除保护用户
+     * 本接口函数用于删除被保护的用户。
+     *
      * @param string $username 保护用户名
      * @return boolean
      */
@@ -426,6 +455,8 @@ class Client
 
     /**
      * 得到受保护的用户名列表
+     * 本接口函数用于获得被保护的用户列表。
+     *
      * @return array
      */
     public function userGetProtected()
@@ -434,5 +465,62 @@ class Client
         return xml_unserialize($response);
     }
 
+    /**
+     * 把重名用户合并到 UCenter
+     * 本接口函数用于把重名的用户合并到 UCenter。
+     *
+     * @param string $oldusername   老用户名
+     * @param string $newusername   新用户名
+     * @param int $uid              用户 ID
+     * @param string $password      密码
+     * @param string $email         电子邮件
+     * @return int
+     */
+    public function userMerge($oldusername, $newusername, $uid, $password, $email)
+    {
+        $response = $this->apiPost('user', 'merge', compact('oldusername', 'newusername', 'uid', 'password', 'email'));
+        if (0 > $response) {
+            switch ($response) {
+                case '-1':
+                    throw new UcException('用户名不合法', -1);
+                    break;
+                case '-2':
+                    throw new UcException('包含不允许注册的词语', -2);
+                    break;
+                case '-3':
+                    throw new UcException('用户名已经存在', -3);
+                    break;
+                default:
+                    throw new UcException('未知错误', $response);
+                    break;
+            }
+        }
+        return (int)$response;
+    }
 
+    /**
+     * 移除重名用户记录
+     * 本接口函数用于移除重名用户记录中的指定记录，如果应用程序已处理完该重名用户，可以执行此接口函数。
+     *
+     * @param string $username
+     */
+    public function userMergeRemove($username)
+    {
+        $this->apiPost('user', 'merge_remove', compact('username'));
+    }
+
+    /**
+     * 获取指定应用的指定用户积分
+     * 本接口函数用于获取指定应用的指定用户积分。
+     *
+     * @param int $appid 应用 ID
+     * @param int $uid 用户 ID
+     * @param int $credit 积分编号
+     * @return int
+     */
+    public function userGetCredit($appid, $uid, $credit)
+    {
+        $response = $this->apiPost('user', 'getcredit', compact('appid', 'uid', 'credit'));
+        return (int)$response;
+    }
 }
